0b0c93d503b7f79486947cf389d587c6fe4fdf08
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-09-26 00:23:11 +0600

    react fontawesome installed and add a icon

--- src/components/Member/Member.js
+++ src/components/Member/Member.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import './Member.css';
 
 const Member = (props) => {
     const { member } = props;
     const { name, country, role, salary, img, age } = member;
     console.log(member);
+    const cart = <FontAwesomeIcon icon={faShoppingCart} />;
     return (
-        <div className="col">
-            <div className="card">
-                <img src={img} className="card-img-top card-image" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <h6>{role}</h6>
-                    <p className="card-text">Country : {country}</p>
-                    <p className="card-text">Age : {age}</p>
-                    <p className="card-text">Salary : {salary}</p>
-            </div>
-    </div>
+      <div className="col">
+        <div className="card">
+          <img src={img} className="card-img-top card-image" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <h6>Role : {role}</h6>
+            <p className="card-text">Country : {country}</p>
+            <p className="card-text">Age : {age}</p>
+            <p className="card-text">Salary : ${salary}</p>
+            <button type="button" class="btn btn-danger">
+                        {cart} Add to cart
+            </button>
+          </div>
         </div>
+      </div>
     );
 };
 
